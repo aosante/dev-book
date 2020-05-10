2c4fb6ee4c738028ec9c745f8bcb52435933d8b9
Andrés Osante <aosantea@ucenfote.ac.cr>
2020-05-09 23:57:43 -0600

    deployment config

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -20,6 +21,14 @@ app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 app.use('/api/auth', auth);
 
-const port = 5000 || process.env.PORT;
+// Serve static assets in prod
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (_, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
